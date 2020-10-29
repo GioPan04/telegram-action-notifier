33e0b6df513e25d947968c1c718ef104442bdb7a
GioPan04 <gioele@pannetto.com>
2020-10-29 13:07:10 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+try {
+    const chatId = core.getInput('bot-chat-id');
+    console.log(`ChatID: ${chatId}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
